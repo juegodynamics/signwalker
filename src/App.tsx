377c3de29ea7f2209f70efcbd719e9b958e70e48
Jacovie Rodriguez <jacovie@alum.mit.edu>
2022-08-17 17:55:33 -0400

    support font

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,21 @@ const theme = createTheme({
       default: "#303030",
     },
   },
+  typography: {
+    fontFamily: [
+      "SuttonSignWritingOneD",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
 });
 
 function App() {
